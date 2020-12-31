5e468a6029976654ac765d496b6f8348a4dbebab
Ibnu Fachrizal <ibnufachrizal63@gmail.com>
2020-12-31 14:51:25 +0700

    Update autorecon.sh

--- autorecon.sh
+++ autorecon.sh
@@ -22,21 +22,19 @@ subdomainPortScanner(){
 }
 subdomainPortScanner
 
-
-
-
 nucleiScan(){
-	nuclei -l $domain/subs/Httprobe.txt -t ~/templates/cves -c 100 -o $domain/recon/CVEs.txt
-	nuclei -l $domain/subs/Httprobe.txt -t ~/templates/vulnerabilities -c 100 -o $domain/recon/Vulners.txt
-	nuclei -l $domain/subs/Httprobe.txt -t ~/templates/security-misconfiguration -c 100 -o $domain/recon/Misconfig.txt
-	nuclei -l $domain/subs/Httprobe.txt -t ~/templates/default-credentials -c 100 -o $domain/recon/DefaultCreds.txt
-	nuclei -l $domain/subs/Httprobe.txt -t ~/templates/files -c 100 -o $domain/recon/Files.txt
-	nuclei -l $domain/subs/Httprobe.txt -t ~/templates/subdomain-takeover -c 100 -o $domain/recon/SubsTakeover.txt
-	nuclei -l $domain/subs/Httprobe.txt -t ~/templates/generic-detections -c 100 -o $domain/recon/Generic.txt
+	nuclei -l $domain/subs/Sub_Port.txt -t ~/templates/cves -c 100 -o $domain/recon/CVEs.txt
+	nuclei -l $domain/subs/Sub_Port.txt -t ~/templates/vulnerabilities -c 100 -o $domain/recon/Vulners.txt
+	nuclei -l $domain/subs/Sub_Port.txt -t ~/templates/security-misconfiguration -c 100 -o $domain/recon/Misconfig.txt
+	nuclei -l $domain/subs/Sub_Port.txt -t ~/templates/default-credentials -c 100 -o $domain/recon/DefaultCreds.txt
+	nuclei -l $domain/subs/Sub_Port.txt-t ~/templates/files -c 100 -o $domain/recon/Files.txt
+	nuclei -l $domain/subs/Sub_Port.txt -t ~/templates/subdomain-takeover -c 100 -o $domain/recon/SubsTakeover.txt
+	nuclei -l $domain/subs/Sub_Port.txt -t ~/templates/generic-detections -c 100 -o $domain/recon/Generic.txt
+}
 nucleiScan
 
 spidering(){
-	cat $domain/subs/Httprobe.txt | gospider |tee $domain/recon/spider.txt
+	cat $domain/subs/Sub_Port.txt | gospider |tee $domain/recon/spider.txt
 	cat $domain/recon/spider.txt | gf xss | tee $domain/recon/xss.txt
 	cat $domain/recon/spider.txt | gf sqli | tee $domain/recon/sqli.txt
 	cat $domain/recon/spider.txt | gf idor | tee $domain/recon/idor.txt
